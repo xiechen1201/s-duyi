7668518349ef2dcf793fa86014fca5c501b3bd57
xiechenLocal <2501869423@qq.com>
2024-05-17 15:42:53 +0800

    feat: update

--- NodeJS/README.md
+++ NodeJS/README.md
@@ -1,1 +1,3 @@
 ## NodeJS
+
+资料 https://gitee.com/dev-edu/frontend-node
--- NodeJS/src/03/index.js
+++
@@ -1,9 +0,0 @@
-/* require("/Users/xiechen/Documents/code-personal/duyi/NodeJS/src/02/index.js"); */
-
-// ========
-
-/* console.log(require);
-console.log(require.resolve("../02/index.js")); */
-// /Users/xiechen/Documents/code-personal/duyi/NodeJS/src/02/index.js
-
-// ===================

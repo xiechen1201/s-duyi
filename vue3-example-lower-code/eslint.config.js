3018072377aeaa75bc1c2d46111c363496d4ae03
xiechenLocal <2501869423@qq.com>
2025-01-14 16:59:16 +0800

    feat: 完成home页面的搭建

--- vue3-example-lower-code/eslint.config.js
+++ vue3-example-lower-code/eslint.config.js
@@ -16,10 +16,17 @@ export default [
 
   ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
   skipFormatting,
+
+  // 覆盖 pluginVue 的规则
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
 ]

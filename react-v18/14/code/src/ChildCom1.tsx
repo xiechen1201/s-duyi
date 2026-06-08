4a7dafc0c5df5d5a2b7dad0c7e1b8872a127a8d0
xiechen@qihang <xiechen@kuaixie.com>
2026-06-08 11:10:31 +0800

    feat: update

--- react-v18/14/code/src/ChildCom1.tsx
+++
@@ -1,9 +0,0 @@
-function ChildCom1(props: { name: string }) {
-  return (
-    <div>
-      这是子组件，<div>名称：{props.name}</div>
-    </div>
-  );
-}
-
-export default ChildCom1;
--- react-v18/14/code/src/ChildCom2.tsx
+++
@@ -1,9 +0,0 @@
-function ChildCom2(props: { age: number }) {
-  return (
-    <div>
-      这是子组件，<div>年龄：{props.age}</div>
-    </div>
-  );
-}
-
-export default ChildCom2;
